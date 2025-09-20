490a091ab2eb3b0639da3430c724e00a158f78ee
NguyenHongHoang10 <24020139@vnu.edu.vn>
2025-09-20 20:17:51 +0700

    Create Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,11 @@
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Hồng Hoàng\t24020139\tK69I-IT4\tNguyenHongHoang10\t24020139@vnu.edu.vn");
+        for(int i=9; i>0; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
